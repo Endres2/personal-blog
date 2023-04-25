afcb3b3bdeff1ad1f082cc38455e24d0860e95a3
Endres2 <andresdhr99@gmail.com>
2023-04-25 00:46:02 -0400

    Create partials, home, contact, about, and compose get routs and layout, create post route to /compose to get input text for new post

--- app.js
+++ app.js
@@ -15,6 +15,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req,res)=>{
+  res.render("home", {homeContent:homeStartingContent});
+})
+app.get("/about", (req,res)=>{
+  res.render("about", {aboutContent:aboutContent});
+})
+app.get("/contact", (req,res)=>{
+  res.render("contact", {contactContent:contactContent});
+})
+app.get("/compose", (req,res)=>{
+  res.render("compose", {route:"/compose"});
+})
+
+app.post("/compose", (req,res)=>{
+  const post = {
+    title:req.body.myPost, 
+    body:req.body.myPostContent
+  }
+  console.log(post)
+  res.redirect("/compose")
+})
+
 
 
 
